8d24a384b04ef5bcbc74bc23fd3d3bef67f66692
Nguyen Nam <nghoofnam@gmail.com>
2025-12-12 00:32:35 +0700

    delete some files

--- VideoStreamingLab.java
+++
@@ -1,6 +0,0 @@
-// VideoStreamingLab.java
-public class VideoStreamingLab {
-  public static void main(String[] args) {
-    System.out.println("RTSP/RTP Java server (lab) - use Server.java to run the server.");
-  }
-}
